f51cdb22a7dd3504722d4dba5538e6a5c8abae1c
Ilario Dal Grande <info@silentfrog.net>
2019-11-15 18:15:35 +0100

    updated working mqtt test

--- mqtt_test.py
+++ mqtt_test.py
@@ -7,10 +7,15 @@ def main():
 
     se = SilentEvents(broker, port)
 
-    se.send_media_playpause({
+    # se.send_media_playpause({
+    #     'domain': "workgroup",
+    #     'name': "pc-ilo"
+    # })
+    se.send_mute({
         'domain': "workgroup",
         'name': "pc-ilo"
-    })
+    },
+    action="false")
 
 
 if __name__ == '__main__':
